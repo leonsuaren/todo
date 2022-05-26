1c1a4bfad12ca188d3204d785d9803830d35eca8
Leon Suarez <leonsua@gmail.com>
2022-05-25 21:41:38 -0400

    added animation to the menu

--- src/sections/menu/Menu.js
+++ src/sections/menu/Menu.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { MenuStyled, MenuHeader, MenuItems, AddTodoPlus, Brand, LogOutButton, LogOutButtonWrapper } from './styled';
+import anime from 'animejs';
+
+import { MenuStyled, MenuHeader, MenuItems, AddTodoPlus, Brand, LogOutButton, LogOutButtonWrapper, AiOutlinePlusStyled, AiOutlineMinusStyled, TodoCategory, CategoryWrapper, AddTodoMinus } from './styled';
+
+const Category = ({ category, ...props }) => {
+  return (
+    <TodoCategory {...props} category={category} />
+  )
+}
 
 export const Menu = () => {
+  const [mouseEnter, setMouseEnter] = useState(false);
   const navigate = useNavigate();
   const username = localStorage.getItem('username');
 
@@ -13,13 +22,49 @@ export const Menu = () => {
     navigate('/login');
   }
 
+  const handleOnAddTodo = () => {
+    let categoryAnimation = anime({
+      targets: '.category-animation',
+      opacity: 1,
+      translateY: [-20, 20],
+      delay: anime.stagger(100, { grid: [1, 7], from: 'first' }),
+      autoplay: false,
+    })
+    let addTodoPlusButtonAnimation = anime({
+      targets: '.add-plus-button-animation',
+      opacity: 0,
+      delay: 700,
+      easing: 'easeInQuart'
+    });
+    categoryAnimation.play();
+    addTodoPlusButtonAnimation.play();
+    console.log('hola')
+  }
+
   return (
     <MenuStyled>
       <MenuHeader>
         <Brand>ToDo App</Brand>
       </MenuHeader>
       <MenuItems>
-        <AddTodoPlus>+</AddTodoPlus>
+        <AddTodoPlus
+          onMouseEnter={() => setMouseEnter(true)}
+          onMouseLeave={() => setMouseEnter(false)}
+          onClick={handleOnAddTodo}
+          className='add-plus-button-animation'
+        >
+          <AiOutlinePlusStyled mouseenter={mouseEnter} />
+        </AddTodoPlus>
+        <CategoryWrapper>
+          <Category className='category-animation' category={'home'} />
+          <Category className='category-animation' category={'work'} />
+          <Category className='category-animation' category={'school'} />
+          <Category className='category-animation' category={'read'} />
+          <Category className='category-animation' category={'weekend'} />
+          <Category className='category-animation' category={'shop'} />
+          <Category className='category-animation' category={'followup'} />
+        </CategoryWrapper>
+        <AddTodoMinus className='animation'><AiOutlineMinusStyled/></AddTodoMinus>
       </MenuItems>
       <LogOutButtonWrapper>
         <LogOutButton onClick={handleOnLogout}>Logout</LogOutButton>
--- src/sections/menu/styled.js
+++ src/sections/menu/styled.js
@@ -1,4 +1,7 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { AiOutlinePlus } from 'react-icons/ai'
+import { AiOutlineMinus } from 'react-icons/ai'
+
 
 export const MenuStyled = styled.div`
   display: grid;
@@ -27,7 +30,7 @@ export const Brand = styled.h3`
 export const MenuItems = styled.div`
   display: grid;
   grid-template-columns: 100%;
-  grid-template-rows: repeat(8, 1fr);
+  grid-template-rows: 10% 40% 10%;
   justify-items: center;
   align-items: center;
   width: 100%; 
@@ -53,13 +56,105 @@ export const AddTodoPlus = styled.button`
   }
 `;
 
+export const AddTodoMinus = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #464849;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  box-shadow: 2px 2px 2px 2px #CCCCCC;
+  color: #FFFFFF;
+  font-size: 3rem;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #242526;
+  }
+`;
+
+export const AiOutlinePlusStyled = styled(AiOutlinePlus)`
+  font-size: ${p => p.mouseenter ? '2.5rem' : '2rem'};
+  color: #FFFFFF;
+`;
+
+export const AiOutlineMinusStyled = styled(AiOutlineMinus)`
+  font-size: ${p => p.mouseenter ? '2.5rem' : '2rem'};
+  color: #FFFFFF;
+`;
+
+export const CategoryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: repeat(7, 13%);
+  justify-items: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const category = ({ category }) => {
+  if (category === 'home' ) {
+    return css`
+      background-color: #cf6327;
+    `
+  }
+  if (category === 'work') {
+    return css`
+      background-color: #59c49a;
+    `
+  }
+  if (category === 'school') {
+    return css`
+      background-color: #2ca4da;
+    `
+  }
+  if (category === 'shop') {
+    return css`
+      background-color: #6271e9;
+    `
+  }
+  if (category === 'weekend') {
+    return css`
+      background-color: #ef938b;
+    `
+  }
+  if (category === 'read') {
+    return css`
+      background-color: #bde1e5;
+    `
+  }
+  if (category === 'followup') {
+    return css`
+      background-color: #246c9d;
+    `
+  }
+}
+
+export const TodoCategory = styled.button`
+  ${category}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  color: #FFFFFF;
+  font-size: 3rem;
+  border: none;
+  cursor: pointer;
+  opacity: 0;
+`;
+
 export const LogOutButtonWrapper = styled.div`
   padding: 5px;
   border-radius: 5px;
   border: 5px solid #464849;
   width: 100%;
   height: 60px;
-  box-shadow: 2px 2px 2px #CCCCCC;
+  box-shadow: 5px 5px 5px #CCCCCC;
+  border-inline: 15px;
 `;
 
 export const LogOutButton = styled.button`
